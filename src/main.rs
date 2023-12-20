d9587f50ac6622103a56a2d6c08025629b1569c4
RustyMaster <dawidmajka@protonmail.com>
2023-12-20 17:42:29 +0100

    AoC Day11 fix

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod day06;
 mod day07;
 mod day08;
 mod day09;
+mod day11;
 mod day13;
 mod day14;
 mod day15;
@@ -23,6 +24,7 @@ fn main() {
         Some("day07") => day07::run(),
         Some("day08") => day08::run(),
         Some("day09") => day09::run(),
+        Some("day11") => day11::run(),
         Some("day13") => day13::run(),
         Some("day14") => day14::run(),
         Some("day15") => day15::run(),
